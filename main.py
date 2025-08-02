d9a5ac5a1cb0083462d8e4ed4b6095e88cd5144f
Biray <games.biray@gmail.com>
2025-08-02 14:04:02 +0300

    Final version

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-from stats import word_count, letter_count
+import sys
+from stats import word_count, letter_count, sorted_letter_count
 
 def get_book_text(f_path):
     with open(f_path) as file:
         file_contents = file.read()
-        return file_contents 
-    
-def main():
-    file_path = "books/frankenstein.txt"
-    book_text = get_book_text(file_path)
+        return file_contents
+
+def print_book(book_text, file_path):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}")
+    print(f"----------- Word Count ----------")
     word_count(book_text)
     letters = letter_count(book_text)
+    print(f"--------- Character Count -------")
+    sorted_letter_count(letters)
+    print("============= END ===============")
 
-    for key, value in letters.items():
-        print(f"'{key}': {value}")
+# Make sure the user provided a path for the text book.
+def main():
+    if len(sys.argv) < 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    book_text = get_book_text(file_path)
+    print_book(book_text, file_path)
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,9 +1,10 @@
+#Calculates the total number of words in a given text.
 def word_count(book_text):
     words = book_text.split()
     w_count = len(words)
-    print(f"{w_count} words found in the document")
+    print(f"Found {w_count} total words")
 
-
+# Counts the characters in a text and stores it in a dictionary
 def letter_count(book_text):
     letters = {}
     book_text = book_text.lower()
@@ -11,3 +12,11 @@ def letter_count(book_text):
     for letter in book_text:
         letters[letter] = letters.get(letter, 0) + 1
     return letters
+
+# Takes a dictionary as input. Sorts it by value then prints the letters
+def sorted_letter_count(letters):
+    sorted_letters = {k:v for k, v in sorted(letters.items(), key=lambda item:item[1], reverse=True)}
+
+    for key, value in sorted_letters.items():
+        if key.isalpha():
+            print(f"{key}: {value}")
